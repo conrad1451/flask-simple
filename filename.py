da181b193aac324419efc98e14104e7410e0df71
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-06-01 02:22:22 -0400

    Prove understanding of gunicorn deployment
    If I understand correctly, the format for the start command is: 
    gunicorn <file name>:<app name>.
    
    If this commit is successfully deployed, given the settings I put on my service for deploying this API, then I have understood correctly

---
+++ filename.py
@@ -0,0 +1,29 @@
+# Filename - server.py
+
+# Import flask and datetime module for showing date and time
+from flask import Flask
+import datetime
+
+x = datetime.datetime.now()
+
+# Initializing flask app
+appname = Flask(__name__)
+
+
+# Route for seeing a data
+@appname.route('/')
+# @appname.route('/data')
+def get_time():
+
+	# Returning an api for showing in reactjs
+	return {
+		'Name':"geek", 
+		"Age":"22",
+		"Date":x, 
+		"programming":"python"
+		}
+
+	
+# Running app
+# if __name__ == '__main__':
+# 	appname.run(debug=True)
