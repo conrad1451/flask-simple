5f33ca859b8c87d76290c9418dd30e173a3de763
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-06-21 20:37:06 -0400

    Changing data served from API

--- server.py
+++ server.py
@@ -17,10 +17,10 @@ def get_time():
 
 	# Returning an api for showing in reactjs
 	return {
-		'Name':"geek", 
-		"Age":"22",
+		'Name':"Conrad", 
+		"Age":"24",
 		"Date":x, 
-		"programming":"python"
+		"programming":"python, but sometimes JavaScript ;)"
 		}
 
 	
